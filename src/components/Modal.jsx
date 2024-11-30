2612aa23957c03d0225e94749e1deccdf6353490
shaxriyor0428 <shaxriyorziyodullayev816@gmail.com>
2024-11-30 12:19:13 +0500

    finish project

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const Modal = ({ children, close }) => {
+  useEffect(() => {
+    document.body.style.overflow = "hidden";
+
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, []);
   return (
     <>
       <div
