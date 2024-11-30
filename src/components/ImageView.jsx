8acee69807de5a084dc3b1bc0561a8436b6d4aed
shaxriyor0428 <shaxriyorziyodullayev816@gmail.com>
2024-11-30 18:58:24 +0500

    media finished

--- src/components/ImageView.jsx
+++ src/components/ImageView.jsx
@@ -16,11 +16,11 @@ const ImageView = ({ data }) => {
         mousewheel={true}
         keyboard={true}
         modules={[Navigation, Pagination, Mousewheel, Keyboard]}
-        className="mySwiper select-none w-[900px] max-lg:w-[700px] max-custom-md:w-[600px] bg-gradient-to-r from-white via-gray-100 to-white dark:from-slate-800 dark:via-slate-700 dark:to-slate-800 rounded-lg shadow-lg overflow-hidden"
+        className="mySwiper select-none w-[900px] max-lg:w-[700px] max-custom-md:w-[600px] max-sm:w-[450px] bg-gradient-to-r from-white via-gray-100 to-white dark:from-slate-800 dark:via-slate-700 dark:to-slate-800 rounded-lg shadow-lg overflow-hidden"
       >
         {data?.images?.map((url, inx) => (
           <SwiperSlide key={inx}>
-            <div className="flex h-[500px] max-lg:h-[450px] max-custom-md:h-[400px]">
+            <div className="flex h-[500px] max-lg:h-[450px] max-custom-md:h-[400px] max-sm:h-[300px]">
               <div className="w-[70%] bg-gray-100 dark:bg-slate-900 flex justify-center items-center">
                 <img
                   src={url}
@@ -29,7 +29,7 @@ const ImageView = ({ data }) => {
                 />
               </div>
 
-              <div className="overflow-hidden w-[30%] bg-white dark:bg-slate-800 text-gray-700 dark:text-gray-200 p-6 flex flex-col justify-center items-center space-y-4 gap-3 max-custom-md:gap-0">
+              <div className="overflow-hidden w-[30%] bg-white dark:bg-slate-800 text-gray-700 dark:text-gray-200 p-6 flex flex-col justify-center items-center space-y-4 gap-3 max-custom-md:gap-0 max-sm:p-0 max-sm:space-y-1">
                 <h1 className="font-oswald text-3xl font-bold text-center text-blue-600 dark:text-blue-400 max-custom-md:text-2xl">
                   {data?.title}
                 </h1>
